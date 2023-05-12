93a9701a957d2a85607567cdd571e0881046e2e5
Jonny <jonnymthdev@gmail.com>
2023-05-12 02:48:33 +0200

    Modification du fragment shader

--- shaders/dep3d.fs
+++ shaders/dep3d.fs
@@ -3,14 +3,13 @@ uniform vec4 couleur;
 uniform sampler2D tex;
 in vec3 vsoNormal;
 in vec2 vsoTexCoord;
+in float vsoDiffuse; // ajout de la variable d'entrée
 out vec4 fragColor;
 
 void main(void) {
-  vec3 N = normalize(vsoNormal);
-  vec3 L = normalize(vec3(0, 0, -1)); /*vers le bas vers la gauche*/
-  float diffuse = dot(N, -L);
-
   vec4 texColor = texture(tex, vsoTexCoord); // Récupère la couleur de la texture aux coordonnées de texture données
 
-  fragColor = vec4((texColor.rgb * diffuse), couleur.a);
+  fragColor = vec4((texColor.rgb * vsoDiffuse), couleur.a);
+
+  fragColor.rgb *= 1.0 - abs(vsoDiffuse); // afficher la valeur de vsoDiffuse dans la couleur
 }
