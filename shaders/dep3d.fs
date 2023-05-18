3cf63f05ebbb365f13c3a8ce18dbdbc0afda101f
jonny mth <jonnymthdev@gmail.com>
2023-05-18 02:33:45 +0200

    ajout d'un 3éme vecteur de lumiere par rapport à l'axe y

--- shaders/dep3d.fs
+++ shaders/dep3d.fs
@@ -8,15 +8,19 @@ void main(void) {
   vec3 N = normalize(vsoNormal);
   
   
-  vec3 L1 = normalize(vec3(0, 0, 1)); 
+  vec3 L1 = normalize(vec3(0, 0, 1)); // lumiere venant de devant
   float diffuse1 = max(dot(N, L1), 0.0);
   vec4 color1 = vec4(texture(tex, vsoTexCoord).rgb * diffuse1, 1.0);
 
   
-  vec3 L2 = normalize(vec3(0,1,0));
+  vec3 L2 = normalize(vec3(0,1,0));// lumiere venant d'en haut
   float diffuse2 = max(dot(N, L2), 0.0);
   vec4 color2 = vec4(texture(tex, vsoTexCoord).rgb * diffuse2 , 1.0);
 
+  vec3 L3 = normalize(vec3(0,-1,0));// lumiere venant d'en bas 
+  float diffuse3 = max(dot(N,L3),0.0);
+  vec4 color3=vec4(texture(tex,vsoTexCoord).rgb * diffuse3 , 1.0); 
+
   
-  fragColor = 0.5 * (color1 + color2);
+  fragColor = 0.5 * (color1 + color2+color3);
 }
