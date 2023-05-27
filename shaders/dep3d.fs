c3facdfb48e06771e15fbfa35047b693eb0a72f2
jonny mth <jonnymthdev@gmail.com>
2023-05-27 03:19:42 +0200

    Modification du shaders pour quil prennent en compte le mapping

--- shaders/dep3d.fs
+++ shaders/dep3d.fs
@@ -1,26 +1,36 @@
 #version 330
+
 uniform sampler2D tex;
+uniform sampler2D nm;
+uniform int use_nm;
+
 in vec2 vsoTexCoord;
 in vec3 vsoNormal;
+
 out vec4 fragColor;
 
 void main(void) {
-  vec3 N = normalize(vsoNormal);
+    // Obtenir la normale de la normal map et l'amplifier
+    vec3 N = vec3(0.0);
+    if (use_nm == 1) {
+        N = texture(nm, vsoTexCoord).rgb * vec3(2.0, 2.0, 2.0) - vec3(1.0, 1.0, 1.0);
+    } else {
+        N = normalize(vsoNormal);
+    }
   
-  
-  vec3 L1 = normalize(vec3(0, 0, 1)); // lumiere venant de devant
-  float diffuse1 = max(dot(N, L1), 0.0);
-  vec4 color1 = vec4(texture(tex, vsoTexCoord).rgb * diffuse1, 1.0);
+    // Calculer l'éclairage pour chaque lumière
+    vec3 L1 = normalize(vec3(0, 0, 1));
+    float diffuse1 = max(dot(N, L1), 0.0);
+    vec4 color1 = vec4(texture(tex, vsoTexCoord).rgb * diffuse1, 1.0);
 
-  
-  vec3 L2 = normalize(vec3(0,1,0));// lumiere venant d'en haut
-  float diffuse2 = max(dot(N, L2), 0.0);
-  vec4 color2 = vec4(texture(tex, vsoTexCoord).rgb * diffuse2 , 1.0);
+    vec3 L2 = normalize(vec3(0,1,0));
+    float diffuse2 = max(dot(N, L2), 0.0);
+    vec4 color2 = vec4(texture(tex, vsoTexCoord).rgb * diffuse2 , 1.0);
 
-  vec3 L3 = normalize(vec3(0,-1,0));// lumiere venant d'en bas 
-  float diffuse3 = max(dot(N,L3),0.0);
-  vec4 color3=vec4(texture(tex,vsoTexCoord).rgb * diffuse3 , 1.0); 
+    vec3 L3 = normalize(vec3(0,-1,0));
+    float diffuse3 = max(dot(N,L3),0.0);
+    vec4 color3=vec4(texture(tex,vsoTexCoord).rgb * diffuse3 , 1.0); 
 
-  
-  fragColor = 0.5 * (color1 + color2+color3);
+    // Combinaison des couleurs éclairées
+    fragColor = 0.5 * (color1 + color2 + color3);
 }
